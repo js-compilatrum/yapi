c050a46f6700409094149bfa80bc9d451bd8f8ba
jacekskrzypacz <jacek.skrzypacz@interia.pl>
2024-11-04 09:26:45 +0100

    Add requirements.txt

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
 from datetime import datetime
+
+from flask import Flask
+import requests
 app = Flask(__name__)
 
 
@@ -7,6 +9,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return f'Hello World! now {datetime.now()}'
 
+@app.route('/bible')
+def bible_words():
+    html = requests.get('https://opoka.org.pl/liturgia/')
+    return html.text
 
 if __name__ == '__main__':
     app.run()
